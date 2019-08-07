8e542e7202f1fcef48e2a7e2fa3c4a045c8984a1
Angel Zaprianov <fire1@abv.bg>
2019-08-07 15:09:49 +0300

    init

--- lib/AnalogButtons.h
+++ lib/AnalogButtons.h
@@ -19,7 +19,7 @@ typedef uint8_t btn;
 #endif
 
 #ifndef ButtonDebounceInterval
-#define ButtonDebounceInterval 5000
+#define ButtonDebounceInterval 15000
 #endif
 
 class AnalogButtons {
@@ -27,7 +27,7 @@ class AnalogButtons {
     btn last = 0;
     uint8_t index = 0;
     int read;
-    unsigned long lastCheck = 0,compare = 0;
+    unsigned long lastCheck = 0, compare = 0;
 
 private:
 
@@ -37,10 +37,24 @@ private:
             Serial.print(F(" Encoder Click "));
         }
 
-        if (last == now && read > 600 && read < 700) {
+/*        if (last == now && read > 600 && read < 700) {
             now = btnEntries;
             Serial.print(F(" Encoder 2 Click "));
+        }*/
+    }
+
+    void detectBlinker() {
+        if (read > 20 && read < 100 && last != now) {
+            now = btnBlinker;
+            Serial.print(F(" Blinker Click "));
         }
+    }
+
+    boolean isDebounce() {
+        if (read > 0) {
+            return currentMillis - lastCheck > ButtonDebounceInterval;
+        }
+        return true;
     }
 
 public:
@@ -48,6 +62,7 @@ public:
     const static btn btnNone = 0;
     const static btn btnEncoder = 1;
     const static btn btnEntries = 2;
+    const static btn btnBlinker = 3;
 
     void begin() {
         pinMode(pinAnalogBt, INPUT_PULLUP);
@@ -55,32 +70,33 @@ public:
 
     void listener() {
 
-        if (currentMillis - lastCheck >= ButtonCheckInterval && index <= ButtonCheckIndexInt) {
-            lastCheck = currentMillis;
-            read = 0;
-            compare += analogRead(pinAnalogBt);
-            index++;
+        if (now == btnNone) {
+            if (currentMillis - lastCheck >= ButtonCheckInterval && index <= ButtonCheckIndexInt && isDebounce()) {
+                lastCheck = currentMillis;
+                read = 0;
+                compare += analogRead(pinAnalogBt);
+                index++;
 
-        }
+            }
 
-        if (index >= ButtonCheckIndexInt && compare > 0) {
-            compare += analogRead(pinAnalogBt);
-            index++;
-            compare = compare / index;
-            index = 0;
+            if (index >= ButtonCheckIndexInt && compare > 0) {
+                compare += analogRead(pinAnalogBt);
+                index++;
+                read = (int)compare / index;
+                index = 0;
 
-            if (compare > 0 && compare < 980) {
-                read = compare;
-                lastCheck = currentMillis + ButtonCheckInterval;
-                detectEncoder();
-                compare = 0;
-
+                if (read > 0 && read < 980) {
+                    lastCheck = currentMillis + ButtonCheckInterval;
+                    detectEncoder();
+                    detectBlinker();
+                    compare = 0;
+                }
             }
         }
     }
 
-    int getReadings(){
-        return  read;
+    int getReadings() {
+        return read;
     }
 
     btn getButton() {
